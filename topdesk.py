4396d6c2f07712af351d7866eb714d9279eec6fb
Stonesth <pierre.thonon@gmail.com>
2023-11-22 15:43:04 +0100

    Change the way of retrived find_element_by_ID and find_element_by_xpath

--- topdesk.py
+++ topdesk.py
@@ -4,6 +4,7 @@ from Tools import tools_v000 as tools
 import os
 from os.path import dirname
 import sys
+from selenium.webdriver.common.by import By
 
 # -7 for the name of this project Topdesk
 #save_path = dirname(__file__)[ : -7]
@@ -25,22 +26,22 @@ def connectViaLink() :
         print("It's a change : " + incidentNumber)
         tools.driver.get("https://nnbe.topdesk.net/tas/secure/newchange?action=lookup&lookup=number&lookupValue="+incidentNumber)
     tools.waitLoadingPageByID("idp-9755e903-8758-4a25-8067-acb2d6341ab6")
-    submit_button = tools.driver.find_element_by_id("idp-9755e903-8758-4a25-8067-acb2d6341ab6")
+    submit_button = tools.driver.find_element(By.ID, "idp-9755e903-8758-4a25-8067-acb2d6341ab6")
     submit_button.click()
 
 def incidentTitle() :
     global incidentTitle
     tools.waitLoadingPageByXPATH("/html/body/div[1]/div/h1/div[1]")
-    incidentTitle = tools.driver.find_element_by_xpath("/html/body/div[1]/div/h1/div[2]").text.encode('ascii', 'ignore')
+    incidentTitle = tools.driver.find_element(By.XPATH, "/html/body/div[1]/div/h1/div[2]").text.encode('ascii', 'ignore')
     print("incidentTitle : " + incidentTitle)
     
     global description_text     # /html/body/div[1]/div/div[3]/div[3]/div[3]/div[1]/div[2]/div[2]/div[2]/div/div/div[2]/div[2]
     
     if (incidentNumber.startswith('I')) :
         tools.waitLoadingPageByXPATH("/html/body/div[1]/div/div[3]/div[3]/div[3]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]")
-        description_text = tools.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[3]/div[3]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]").text.encode('ascii', 'ignore')
+        description_text = tools.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[3]/div[3]/div[1]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]").text.encode('ascii', 'ignore')
     else :
         tools.waitLoadingPageByXPATH("/html/body/div/div/div[3]/div[3]/div[3]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]")
-        description_text = tools.driver.find_element_by_xpath("/html/body/div/div/div[3]/div[3]/div[3]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]").text.encode('ascii', 'ignore')
+        description_text = tools.driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[3]/div[3]/div[2]/div[2]/div[2]/div/div/div[1]/div[2]").text.encode('ascii', 'ignore')
     
     print("description_text : " + description_text)
